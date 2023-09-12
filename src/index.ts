2dc93be3f755d024b021f1a9b2baed85b2b6ba58
arleysouza <arley.ferreira.souza@gmail.com>
2023-09-12 19:13:27 -0300

    draft

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import routes from './routes';
 import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 
 // será usado 3000 se a variável de ambiente não tiver sido definida
@@ -8,6 +9,8 @@ const PORT = process.env.PORT || 3000;
 const app = express(); // cria o servidor e coloca na variável app
 // suportar parâmetros JSON no body da requisição
 app.use(express.json());
+//aceitar requisições de outros domínios
+app.use(cors()); 
 // inicializa o servidor na porta especificada
 app.listen(PORT, () => {
     console.log(`Rodando na porta ${PORT}`);
